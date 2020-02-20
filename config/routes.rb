fc266acc71b472ce5f9267cce215bd8d44d7b542
tamr9678 <tamr9678@gmail.com>
2020-02-20 22:10:16 +0900

    ユーザー登録画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "events#index"
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+
+  devise_scope :user do
+    get 'new', to: 'users/registrations#new'
+    post 'create', to: 'users/registrations#create'
+    get 'complete', to: 'users/registrations#complete'
+  end
+
   resources :events , only: [:index]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_105214) do
+ActiveRecord::Schema.define(version: 2020_02_20_120611) do
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "introduction"
     t.text "place"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
