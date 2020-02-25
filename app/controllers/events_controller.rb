bcdbedda5a1472a320f521ca30fd278739bb999b
tamr9678 <tamr9678@gmail.com>
2020-02-26 01:51:20 +0900

    イベント投稿機能

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,18 @@
 class EventsController < ApplicationController
   def index
   end
-end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.create(event_params)
+    redirect_to root_path
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:title, :place, :event_start_at, :event_end_at, :expire_at, :discription, :capacity)
+    end
+  end
