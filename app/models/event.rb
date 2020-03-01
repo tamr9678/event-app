865e099fb89e2c4ed9b99b8028592e16ba77d834
tamr9678 <tamr9678@gmail.com>
2020-03-02 08:27:18 +0900

    開催日時と締め切りのvalidation追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,10 +5,15 @@ class Event < ApplicationRecord
   validates :title,          presence: true, length: {maximum: 50}
   validates :place,          presence: true, length: {maximum: 50}
   validates :event_start_at, presence: true
-  validates :event_end_at,   presence: true
   validates :expire_at,      presence: true
   validates :discription,    presence: true, length: {maximum: 500}
   validates :capacity,       presence: true
+  validate :start_expire_check
+
+  def start_expire_check
+    errors.add(:expire_at, "の日時は開催日時以前に設定してください。") unless
+    self.event_start_at > self.expire_at
+  end
 
   # assosiation
   belongs_to :organizer, class_name: "User"
