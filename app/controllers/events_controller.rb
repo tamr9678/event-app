4457a17db52b49a3c41d898315cc75b8c4651ae3
tamr9678 <tamr9678@gmail.com>
2020-03-02 05:32:30 +0900

    お気に入り件数表示

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,8 +22,7 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
     @participants = Participant.where(event_id: params[:id])
-    
-    
+    @favorites = Favorite.where(event_id: params[:id])
   end
 
   private
