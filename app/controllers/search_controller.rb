d008cf20d9a0b252e86d83f9c828905a4091cff6
tamr9678 <tamr9678@gmail.com>
2020-03-09 15:19:03 +0900

    検索条件”過去に開催されたイベントを表示”を追加

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,10 @@ class SearchController < ApplicationController
   def index
     if params[:search]
       @events =  Event.where(['title LIKE ?', "%#{params[:search]}%"])
+      @past_event = ActiveRecord::Type::Boolean.new.cast(params[:past_event])
+      if !ActiveRecord::Type::Boolean.new.cast(params[:past_event])
+        @events.where!('event_start_at > ?', Time.current)
+      end
     else
       @events = Event.all
     end
